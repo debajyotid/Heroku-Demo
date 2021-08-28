d91016f8ace18ca741eda704c9453416027971c8
debajyotid <31445120+debajyotid@users.noreply.github.com>
2021-08-28 11:38:52 +0530

    Delete request.py

--- request.py
+++
@@ -1,9 +0,0 @@
-"""
-This file is used to demonstrate the capability to invoke the Flask based WebAPI locally, and passing the features via a url, w/o having to render and access the index.html via a browser
-"""
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
-
-print(r.json())
